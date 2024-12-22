583afff277ca41cfeada510258a00c29ccc9b172
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-22 20:21:41 +0530

    String Class

---
+++ string.java
@@ -0,0 +1,21 @@
+
+class string{
+    public static void main(String[] args){
+        String str = "Betty got a bit of bitter better butter";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='b'){
+                System.out.print("Index of " + str.charAt(i) + " = " + i + "\t");
+            }
+            else if(str.charAt(i)=='t'){
+                System.out.print("Index of " + str.charAt(i) + " = " + i + "\t");
+            }
+        }
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==){
+                System.out.println();
+            }
+            System.out.println(str.length());
+        }
+    }
+}
