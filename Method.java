edcd9876e9b7e1953c088b942234f25e6d7f5b57
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-22 20:20:13 +0530

    Method In Java

--- Method.java
+++ Method.java
@@ -1,4 +1,5 @@
-
+// Method 
+/*
 import java.util.Scanner;
 
 class Method{
@@ -19,3 +20,78 @@ class Method{
         obj.add();
     }
 }
+*/
+
+/*
+import java.util.Scanner;
+
+class Method{
+    public void Sum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number :");
+        int num = sc.nextInt();
+        if(num % 2 == 0){
+            System.out.println("Number is Even");
+        }else{
+            System.out.println("Number is Odd");
+        }
+    }
+    public static void main(String[] args){
+        Method object = new Method();
+        object.Sum();
+    }
+}
+*/
+
+
+/*
+import java.util.Scanner;
+
+public class Method {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check if it's prime: ");
+        int number = scanner.nextInt();
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false; 
+        } else {
+            for (int i = 2; i < number; i++) { 
+                if (number % i == 0) {
+                    isPrime = false; 
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+class Method{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.println("Number is Prime");
+        }else{
+            System.out.println("Number Not is Prime");
+        }
+    }
+}
+*/
+
+
