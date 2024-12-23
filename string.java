4f0e753110f1a413a95499bde9ba6404ce8eab95
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-23 21:30:11 +0530

    String

--- string.java
+++ string.java
@@ -1,4 +1,5 @@
-
+// 1 - (a)
+/*
 class string{
     public static void main(String[] args){
         String str = "Betty got a bit of bitter better butter";
@@ -10,12 +11,37 @@ class string{
                 System.out.print("Index of " + str.charAt(i) + " = " + i + "\t");
             }
         }
+    }
+}
+*/
 
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)==){
-                System.out.println();
-            }
-            System.out.println(str.length());
+// 1 - (b)
+/*
+class string{
+    public static void main(String[] args){
+        String str = "Betty got a bit of bitter better butter";
+
+        String[] str1 = str.split(" ");
+        for(int i=0;i<str1.length;i++){
+            System.out.println(str1[i] + " =  " + str1[i].length());
         }
     }
 }
+*/
+
+// 1 - (c)
+
+
+
+// 5
+/*
+import java.util.Scanner;
+
+class Str{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+    }
+}
+*/
+
