ee805ad488f9de9b3f68fe0f315fa758eee06419
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-01-06 00:06:46 +0530

    String Task

---
+++ StringTask.java
@@ -0,0 +1,9 @@
+
+public class StringTask {
+    public static void main(String[] args) {
+        String str = "this is a test string";
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.split(""));
+        }
+    }
+}
