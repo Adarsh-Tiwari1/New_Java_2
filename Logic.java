e1e48977ac505cf17a593ab7a26c2e63c14c2e8a
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-01-07 13:40:26 +0530

    Logic Question

---
+++ Logic.java
@@ -0,0 +1,35 @@
+/*
+class Logic{
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("if part");
+        }else{
+            System.out.println("else part");
+        }
+    }
+}
+*/
+
+/*
+class Logic{
+    public static void main(String[] args) {
+        if(false){
+            System.out.println("if part");
+        }else{
+            System.out.println("else part");
+        }
+    }
+}
+*/
+
+class Logic{
+    public static void main(String[] args) {
+        int x=0;
+        int y=0;
+        if(x==y){
+            System.out.println("x==y");
+        }else{
+            System.out.println("not");
+        }
+    }
+}
