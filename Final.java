bc63f3980139c48aadce8f3f8d6cc31a71d55f12
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-01-07 13:39:36 +0530

    final keyword in java

---
+++ Final.java
@@ -0,0 +1,21 @@
+//  String final class hai .
+
+/*
+final class Parent{
+    public  void show(){
+        System.out.println("This is Parent class show method...");
+    }
+}
+class Final extends Parent{ // final can't override .
+    public void show(){
+        System.out.println("This is child class method...");
+    }
+    public static void main(String[] args) {
+        Parent p1 = new Final();
+        p1.show();
+    }
+}
+*/
+
+// final method ko overload kar sakte hai.
+
