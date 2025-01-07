41085c511a5e63bdad43bfeddbc9d9e394450e86
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-01-08 01:22:30 +0530

    overriding method

---
+++ Overriding.java
@@ -0,0 +1,20 @@
+// signature : method name,method access specifier,same argument , must be same , return time ve same 
+/*
+class Parent{
+    public void show(){
+        System.out.println("This is Parent class show method...");
+    }
+}
+class Overriding extends Parent{
+    public void show(){
+        System.out.println("This is child class method...");
+    }
+    public static void main(String[] args) {
+        Parent p1 = new Overriding();
+        p1.show();
+    }
+}
+*/
+
+
+    
