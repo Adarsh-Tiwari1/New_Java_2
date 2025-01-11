745cd7519d992707be2cdba93d35bafaf9cb9cf0
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-01-11 19:40:00 +0530

    String task

---
+++ StrTask6.java
@@ -0,0 +1,10 @@
+/*
+class StrTask6 {
+    public static void main(String[] args) {
+        String str = "Geeks Gor Geeks";
+        str = str.replace("Gor","For");
+        System.out.println(str);
+
+    }
+}
+*/
