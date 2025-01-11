a7fc4b03004c5141cfac52b942b5225bac614d31
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-01-11 19:28:29 +0530

    loop task

---
+++ LoopTask.java
@@ -0,0 +1,46 @@
+/*
+class LoopTask {
+    public static void main(String[] args) {
+        for(int i=100;i>=1;i=i-3){
+            System.out.print(i+" ");
+        }
+    }
+}
+*/
+
+/*
+class LoopTask{
+    public static void main(String[] args) {
+        for(int i=-5;i<=5;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
+*/
+
+/*
+class LoopTask{
+    public static void main(String[] args) {
+        for(int i=80;i>=70;i--){
+            if(i%2!=0){
+                System.out.println("skip");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
+*/
+
+/*
+class LoopTask{
+    public static void main(String[] args) {
+        for(int i=10;i<=20;i++){
+            if(i%2==0){
+                int result = i*i;
+                System.out.println(i+" Square is : " + result);
+            }
+        }
+    }
+}
+*/
