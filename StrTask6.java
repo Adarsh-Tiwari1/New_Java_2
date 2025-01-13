a58c3f8c6b24e4853c3904c6b3c800a86998c48c
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-01-14 00:51:53 +0530

    string task

--- StrTask6.java
+++ StrTask6.java
@@ -8,3 +8,46 @@ class StrTask6 {
     }
 }
 */
+
+/*
+class StrTask6{
+    public static void main(String[] args) {
+        String str = "Geeks";
+        str = str.replace("G","g");
+            System.out.println(str);
+    }
+}
+*/
+
+/*
+class StrTask6 {
+    public static void main(String[] args) {
+        String str = "00000123569";
+        char[] ch = str.toCharArray();
+        for(int i=0; i<ch.length; i++) {
+            if(ch[i]=='0') {
+            } else{
+                System.out.print(ch[i]);
+            }
+        }
+        System.out.println();
+    }
+}
+*/
+
+/*
+class StrTask6 {
+    public static void main(String[] args) {
+        String str = "000012356090";
+        char[] ch = str.toCharArray();
+        for(int i=0; i<ch.length; i++) {
+            if(ch[i]=='0') {
+            } else{
+                System.out.print(ch[i]);
+            }
+        }
+        System.out.println();
+    }
+}
+*/
+
