acbba255817f10c4fe27d0d343259a85e8d5d0e6
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-07 18:06:08 +0530

    sumTwoNumber

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class sum {
+    public static void main(String args[]){
+         int a = 10,b=20 ;
+        int sum =a+b;
+        int multi = a*b;
+        int divide = a/b;
+        System.out.println(divide); 
+        System.out.println(sum); 
+        System.out.println(multi); 
+        
+    
+    }
+    
+}
