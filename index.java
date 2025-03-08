2040172572a541a73be7592b6451f19a3df6fff4
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-09 01:59:07 +0530

    IndexValue

---
+++ index.java
@@ -0,0 +1,7 @@
+class index{
+    public static void main(String args[]){
+        System.out.println("hello word");
+        System.out.println("jai shree ram");
+        System.out.println("adarsh tiwari");
+    }
+}
