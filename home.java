9c73a5414cdf7ba6b146a04173949de8c5b03d1e
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-09 02:00:46 +0530

    Home.java

---
+++ home.java
@@ -0,0 +1,10 @@
+public class home {
+    public static void main(String args[]){
+      int a =20;
+       a=30;
+       String name ="adarsh";
+       System.out.println(name);
+      System.out.println(a);
+    }
+    
+}
