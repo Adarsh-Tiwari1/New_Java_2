24e5527b8fd14cf85d65fc5306ed4bec58573832
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-08 22:02:04 +0530

    .JAVA

---
+++ pawan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class pawan {
+    public static void main(String args[]){
+    int a,b;
+    System.out.println("enter two number");
+    Scanner obj = new Scanner(System.in);
+   a=obj. nextInt();
+   b=obj.nextInt();
+   System.out.println("addition  "+(a+b));
+   System.out.println("substraction  "+(a-b));
+   System.out.println("multiplication  "+(a*b));
+}
+}
