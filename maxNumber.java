88a715d75ccda1164de3a558b3515a6d68cee596
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-10 18:06:43 +0530

    MaxNumber

---
+++ maxNumber.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+public class maxNumber {
+    public static void main(String args[]){
+        int a=20,b=30,c=50;
+        if(a>b){
+            if(a>c)
+            System.out.println(a);
+            else
+            System.out.println(c);
+        }
+        if(b>c)
+        System.out.println(b);
+        else
+        System.out.println(c);
+
+        
+    }
+}
